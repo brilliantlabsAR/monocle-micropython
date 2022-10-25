435af4565dec14533f1402fb545b0f40e2ae50fd
Josuah Demangeon <me@josuah.net>
2022-10-25 18:29:41 -0400

    add more logging to the python serial console script

--- serial_console.py
+++ serial_console.py
@@ -19,7 +19,6 @@ UART_SERVICE_UUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
 UART_RX_CHAR_UUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 UART_TX_CHAR_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
-
 # TIP: you can get this function and more from the ``more-itertools`` package.
 def sliced(data: bytes, n: int) -> Iterator[bytes]:
     """
@@ -39,6 +38,7 @@ async def uart_terminal():
         # This assumes that the device includes the UART service UUID in the
         # advertising data. This test may need to be adjusted depending on the
         # actual advertising data supplied by the device.
+        print(f"uuids={adv.service_uuids}")
         return UART_SERVICE_UUID.lower() in adv.service_uuids
 
     device = await BleakScanner.find_device_by_filter(match_nus_uuid)
@@ -48,7 +48,7 @@ async def uart_terminal():
         sys.exit(1)
 
     def handle_disconnect(_: BleakClient):
-        print("Device was disconnected, goodbye.")
+        print("Device was disconnected.")
         # cancelling all tasks effectively ends the program
         for task in asyncio.all_tasks():
             task.cancel()
