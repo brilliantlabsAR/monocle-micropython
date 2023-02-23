33fe93e422c6006e9716dc52bf2c8d9417f5519f
Raj Nakarja <raj@siliconwitchery.com>
2023-02-23 16:45:12 +0100

    Added storage read function storage write WIP

--- modules/storage.c
+++ modules/storage.c
@@ -23,32 +23,92 @@
  */
 
 #include <string.h>
+#include <math.h>
 #include "monocle.h"
 #include "py/mphal.h"
 #include "py/runtime.h"
 
 static const size_t reserved_64k_blocks_for_fpga_bitstream = 7;
 
-STATIC mp_obj_t storage_read(mp_obj_t file, mp_obj_t offset, mp_obj_t length)
+static size_t fpga_bitstream_programmed_bytes = 0;
+
+STATIC mp_obj_t storage_read(mp_obj_t file, mp_obj_t length, mp_obj_t offset)
 {
     const char *filename = mp_obj_str_get_str(file);
 
+    // TODO handle offsets or lengths which are too large
+
+    // TODO allow optional length value?
+
+    // TODO allow optional offset value?
+
     if (strcmp(filename, "FPGA_BITSTREAM") == 0)
     {
+        uint32_t address = mp_obj_get_int(offset);
 
-        return mp_const_none;
+        uint8_t read[] = {bit_reverse(0x03),
+                          bit_reverse(address >> 16),
+                          bit_reverse(address >> 8),
+                          bit_reverse(address)};
+        spi_write(FLASH, read, sizeof(read), true);
+
+        uint8_t buffer[mp_obj_get_int(length)];
+
+        spi_read(FLASH, buffer, mp_obj_get_int(length));
+
+        return mp_obj_new_bytes(buffer, mp_obj_get_int(length));
     }
 
     return mp_const_notimplemented;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_3(storage_read_obj, storage_read);
 
-STATIC mp_obj_t storage_append(mp_obj_t file, mp_obj_t data)
+STATIC mp_obj_t storage_append(mp_obj_t file, mp_obj_t bytes)
 {
     const char *filename = mp_obj_str_get_str(file);
 
     if (strcmp(filename, "FPGA_BITSTREAM") == 0)
     {
+        size_t length;
+        const char *data = mp_obj_str_get_data(bytes, &length);
+
+        if (fpga_bitstream_programmed_bytes + length >
+            0x10000 * reserved_64k_blocks_for_fpga_bitstream)
+        {
+            mp_raise_ValueError(MP_ERROR_TEXT(
+                "data will overflow reserved FPGA bitstream space"));
+        }
+
+        uint8_t write_enable[] = {bit_reverse(0x06)};
+        spi_write(FLASH, write_enable, sizeof(write_enable), false);
+
+        uint8_t page_program[] =
+            {bit_reverse(0x02),
+             bit_reverse(fpga_bitstream_programmed_bytes >> 16),
+             bit_reverse(fpga_bitstream_programmed_bytes >> 8),
+             bit_reverse(fpga_bitstream_programmed_bytes)};
+        spi_write(FLASH, page_program, sizeof(page_program), true);
+        spi_write(FLASH, (uint8_t *)data, length, true);
+
+        fpga_bitstream_programmed_bytes += length;
+
+        // If the currently written data isn't aligned to a page boundary
+        // if (fpga_bitstream_programmed_bytes % 256)
+        // {
+        //     size_t remaining_bytes_in_page =
+        //         256 - (fpga_bitstream_programmed_bytes % 256);
+
+        //     if (length <= remaining_bytes_in_page)
+        //     {
+
+        //     }
+
+        //     bytes_written += remaining_bytes_in_page;
+        // }
+
+        // Write the remaining data from the page boundary in 256byte chunks
+
+        // Write a partial page
 
         return mp_const_none;
     }
@@ -90,6 +150,8 @@ STATIC mp_obj_t storage_delete(mp_obj_t file)
                 mp_hal_delay_ms(10);
             }
         }
+
+        fpga_bitstream_programmed_bytes = 0;
 
         return mp_const_none;
     }
