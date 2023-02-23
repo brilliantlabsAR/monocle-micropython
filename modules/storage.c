09216c8d363b3856c786e938ca6e7554be073b93
Raj Nakarja <raj@siliconwitchery.com>
2023-02-23 15:35:33 +0100

    Added storage erase function Added placeholders for read and append

--- modules/storage.c
+++ modules/storage.c
@@ -22,18 +22,88 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include <string.h>
 #include "monocle.h"
+#include "py/mphal.h"
 #include "py/runtime.h"
 
-STATIC mp_obj_t storage_read(void)
+static const size_t reserved_64k_blocks_for_fpga_bitstream = 7;
+
+STATIC mp_obj_t storage_read(mp_obj_t file, mp_obj_t offset, mp_obj_t length)
 {
-    return mp_const_none;
+    const char *filename = mp_obj_str_get_str(file);
+
+    if (strcmp(filename, "FPGA_BITSTREAM") == 0)
+    {
+
+        return mp_const_none;
+    }
+
+    return mp_const_notimplemented;
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_0(storage_read_obj, storage_read);
+STATIC MP_DEFINE_CONST_FUN_OBJ_3(storage_read_obj, storage_read);
+
+STATIC mp_obj_t storage_append(mp_obj_t file, mp_obj_t data)
+{
+    const char *filename = mp_obj_str_get_str(file);
+
+    if (strcmp(filename, "FPGA_BITSTREAM") == 0)
+    {
+
+        return mp_const_none;
+    }
+
+    return mp_const_notimplemented;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_2(storage_append_obj, storage_append);
+
+STATIC mp_obj_t storage_delete(mp_obj_t file)
+{
+    const char *filename = mp_obj_str_get_str(file);
+
+    if (strcmp(filename, "FPGA_BITSTREAM") == 0)
+    {
+        for (size_t i = 0; i < reserved_64k_blocks_for_fpga_bitstream; i++)
+        {
+            uint8_t write_enable[] = {bit_reverse(0x06)};
+            spi_write(FLASH, write_enable, sizeof(write_enable), false);
+
+            uint32_t address_24bit = 0x10000 * i;
+            uint8_t block_erase[] = {bit_reverse(0xD8),
+                                     bit_reverse(address_24bit >> 16),
+                                     0, // Bottom bytes of address are always 0
+                                     0};
+            spi_write(FLASH, block_erase, sizeof(block_erase), false);
+
+            while (true)
+            {
+                uint8_t status[] = {bit_reverse(0x05)};
+                spi_write(FLASH, status, sizeof(status), true);
+                spi_read(FLASH, status, sizeof(status));
+
+                // If no longer busy
+                if ((status[0] & bit_reverse(0x01)) == 0)
+                {
+                    break;
+                }
+
+                mp_hal_delay_ms(10);
+            }
+        }
+
+        return mp_const_none;
+    }
+
+    return mp_const_notimplemented;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(storage_delete_obj, storage_delete);
 
 STATIC const mp_rom_map_elem_t storage_module_globals_table[] = {
 
     {MP_ROM_QSTR(MP_QSTR_read), MP_ROM_PTR(&storage_read_obj)},
+    {MP_ROM_QSTR(MP_QSTR_append), MP_ROM_PTR(&storage_append_obj)},
+    {MP_ROM_QSTR(MP_QSTR_delete), MP_ROM_PTR(&storage_delete_obj)},
+    {MP_ROM_QSTR(MP_QSTR_FPGA_BITSTREAM), MP_ROM_QSTR(MP_QSTR_FPGA_BITSTREAM)},
 };
 STATIC MP_DEFINE_CONST_DICT(storage_module_globals, storage_module_globals_table);
 
