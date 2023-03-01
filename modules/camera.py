54ab6e554dac7539aaa449d0018f7d6bf00f6b38
Josuah Demangeon <me@josuah.net>
2023-03-01 14:41:30 -0500

    camera.py: add a WIP function for replay()

--- modules/camera.py
+++ modules/camera.py
@@ -39,7 +39,22 @@ def overlay(enable):
     fpga.write(0x1005, "")
     fpga.write(0x3005, "")
   else:
-    fpga.write(0x3004, "")
+    fpga.write(0x3004, "") # this discards the record buffer
     fpga.write(0x1004, "")
     time.sleep_ms(100);
     __camera.sleep()
+
+def record(enable):
+  if enable:
+    __camera.wake()
+    fpga.write(0x1005, b'') # record on
+  else:
+    fpga.write(0x1004, b'') # record off
+    __camera.sleep()
+
+def replay():
+  record(False)
+  overlay(True)
+  fpga.write(0x3007, b'') # replay once
+  time.sleep(4)
+  overlay(False)
