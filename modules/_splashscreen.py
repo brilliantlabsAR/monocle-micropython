3ffb32dfe9af263bac4349bdfd787b41174118aa
Josuah Demangeon <me@josuah.net>
2023-05-25 04:39:25 -0400

    `del t` only needs to run if `t` is assigned to something

--- modules/_splashscreen.py
+++ modules/_splashscreen.py
@@ -3,7 +3,7 @@ import fpga, display
 if fpga.read(1, 4) == b"Mncl":
     t = display.Text("MONOCLE", 320, 200, display.WHITE, justify=display.MIDDLE_CENTER)
     display.show(t)
+    del t
 
-del t
 del fpga
 del display
