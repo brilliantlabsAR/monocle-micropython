d721ddca9ddf1939fbdc7db971fa867d9bcfee9c
Josuah Demangeon <me@josuah.net>
2023-06-17 16:12:07 -0400

    camera: add primitive support for capture

--- modules/camera.py
+++ modules/camera.py
@@ -31,42 +31,64 @@ RGB = "RGB"
 YUV = "YUV"
 JPEG = "JPEG"
 
-__overlay_state = False
+
+__image = __fpga.read(0x0001, 4)
+__status1 = __fpga.read(0x1000, 1)[0] & 0x10
+__status2 = __fpga.read(0x5000, 1)[0] & 0x10
+if __status1 != 16 or __status2 != 16: # or __image != b"Mncl":
+    raise (NotImplementedError("camera driver not found on FPGA"))
 
 
-def capture(url):
-    raise NotImplementedError
+_capture_on = False
+_capture_y = 0
 
 
-def overlay(enable=None):
-    if enable == None:
-        global __overlay_state
-        return __overlay_state
-    if enable == True:
-        __fpga.write(0x4404, "")
-        __time.sleep_ms(100)
-        __camera.wake()
-        __fpga.write(0x1005, "")
-        __fpga.write(0x3005, "")
-        __overlay_state = True
-    else:
-        __fpga.write(0x3004, "")
-        __fpga.write(0x1004, "")
-        __time.sleep_ms(100)
-        __camera.sleep()
-        __overlay_state = False
+def capture(x, y, format):
+    global _capture_y
+    global _capture_on
 
-
-def output(x, y, format):
-    raise NotImplementedError
-
-
-def zoom(multiplier):
     __camera.wake()
+    __fpga.write(0x1005, b'')
+    __fpga.write(0x3005, b'')
+
     __time.sleep_ms(100)
-    __camera.zoom(multiplier)
-    if not overlay():
-        __camera.sleep()
+    __camera.write(0x3002, 0x00) # SYSTEM_RESET_2 - set everything on
+    __time.sleep_ms(100)
+    __camera.write(0x3006, 0xFF) # CLOCK_ENABLE_2 - enable all clocks
+    __time.sleep_ms(100)
+    __camera.write(0x3821, 0x27) # TIMING_TC_REG_21 - enable JPEG, binning, mirror
+    __time.sleep_ms(100)
+    __camera.write(0x4300, 0x30) # FORMAT_CONTROL_0 - YUV422, YUYV
+    __time.sleep_ms(100)
+    __camera.write(0x501f, 0x00) # FORMAT_MUX_CONTROL - select YUV422
+    __time.sleep_ms(100)
+    __camera.write(0x4713, 0x02) # JPG_MODE_SELECT - compression mode 2
+    __time.sleep_ms(100)
+    __camera.write(0x460c, 0x22) # VFIFO_CTRL0C - undocumented bit
+    __time.sleep_ms(100)
+    __camera.write(0x3824, 0x04) # DVP_PCLK - clock divider value
+    __time.sleep_ms(100)
+    __camera.write(0x460b, 0x35) # DEBUG_MODE - undocumented
+    __time.sleep_ms(100)
+    __fpga.write(0x5004, b'')
+    __time.sleep_ms(100)
+
+    _capture_on = True
+    _capture_y = y
+
+
+def capture_off():
+    __fpga.write(0x1004, "") # OVCAM_PAUSE_REQ
+    __camera.sleep()
+
+
+def read(bytes=254):
+    if not _capture_on:
+        raise ValueError("no ongoing capture()")
+    if bytes > 254:
+        raise ValueError("at most 254 bytes")
+    __time.sleep_ms(10)
+    return __fpga.read(0x5005, bytes)
 
 
 def record(enable):
