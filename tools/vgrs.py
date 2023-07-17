9c5ff6919e3741ad4dd5218efc831f16201bc52a
Josuah Demangeon <me@josuah.net>
2023-07-17 06:00:41 -0400

    virt-vgrs: support most if not all of the RVGR format

--- tools/vgrs.py
+++ tools/vgrs.py
@@ -5,60 +5,120 @@ WIDTH = 640
 HEIGHT = 400
 DEPTH = 3
 
-# color scheme to update manually (for now) according to the data
+# the first line has the color palette in a list of "key=value" strings
+# separated by space
 palette = {}
-palette[0x0FF] = b'\xFF\x00\x00' # Color 0
-palette[0x1FF] = b'\xFF\x66\x00' # Color 1
-palette[0x2FF] = b'\xFF\xFF\x00' # Color 2
-palette[0x3FF] = b'\x00\xFF\x00' # Color 3
-palette[0x4FF] = b'\x00\x00\xFF' # Color 4
-palette[0x5FF] = b'\xFF\x00\x66' # Color 5
-palette[0x6FF] = b'\xFF\x00\xAA' # Color 6
+buf = bytes.fromhex(sys.stdin.readline().rstrip())
+it = iter(buf)
 
-# read input hexadecimal string and decode it into a bytearray
+# Decode the RGB palette coming as first line
+i = 0
+assert(next(it) == 0x00) # Skip address field, first byte
+assert(next(it) == 0x00) # Skip address field, second byte
+while True:
+    try:
+        palette[i] = bytes((next(it), next(it), next(it)))
+    except StopIteration:
+        break
+    i += 1
+print(palette)
+
+# Read input hexadecimal string and decode it into a bytearray
 buf = bytearray()
 for line in sys.stdin.readlines():
     buf.extend(bytes.fromhex(line.rstrip()))
 
-# prepare a memory canvas to store the image to be drawn
+# Prepare a memory canvas to store the image to be drawn
 rgb = bytearray(WIDTH * HEIGHT * DEPTH)
 
-# reset the current values for the pointer that draws onto the RGB canvas
+# Cursor drawing onto the RGB canvas
 cur_x = 0
 cur_y = 0
 cur_color = b'\x00\x00\x00'
 
+def cmd_PutSpan(arg):
+    global cur_x
+    global cur_y
+    global cur_color
+
+    for i in range(arg // 8):
+        rgb[cur_y * WIDTH * DEPTH + cur_x * DEPTH + 0] = cur_color[0]
+        rgb[cur_y * WIDTH * DEPTH + cur_x * DEPTH + 1] = cur_color[1]
+        rgb[cur_y * WIDTH * DEPTH + cur_x * DEPTH + 2] = cur_color[2]
+        cur_x += 1
+
+def cmd_SetColor(arg):
+    global cur_color
+
+    # Update the current color using an index from the palette
+    cur_color = palette[arg]
+
+def cmd_SkipColumns(arg):
+    global cur_x
+
+    # No data for these many pixels
+    cur_x += arg // 16
+
+def cmd_JumpToLine(arg):
+    global cur_x
+    global cur_y
+
+    # No data for these many lines
+    cur_x = 0
+    cur_y = arg
+
+def cmd_NextLinePos(arg):
+    global cur_x
+    global cur_y
+
+    # Jump to the column given on the next row
+    cur_x = arg // 16
+    cur_y += 1
+
 # iterate through pair of bytes, forming an uint16_t containing a command
 it = iter(buf)
-for b1 in it:
-    b2 = next(it)
-    num = ((b1 & 0x0F) << 8) + b2
+while True:
+    try:
+        b1 = next(it)
+        b2 = next(it)
+    except StopIteration:
+        break
+    num = (b1 << 8) | b2
+    cmd = (b1 & 0b11110000) >> 4
+
+    print(f"{b1:02X} {b2:02X} ", end="")
 
     # dispatch the command to draw on the canvas accordingly
 
-    if b1 & 0xF0 == 0x00:
-        cur_color = palette[num]
-        print(f'{b1:02X} {b2:02X} SetColor: {cur_color} (0x{num:X})')
+    if cmd & 0b1000 == 0:
+        arg1 = (num & 0b_01111111_00000000) >> 8
+        arg2 = (num & 0b_00000000_11111111)
+        cmd_SetColor(arg1)
+        cmd_PutSpan(arg2)
+        print(f"SetColor: {list(cur_color)}; PutSpan: 0x{arg2:X}")
 
-    elif b1 & 0xF0 == 0x80:
-        cur_x += num // 8
-        print(f'{b1:02X} {b2:02X} Skip: 0x{num:X}')
+    elif cmd == 0x8 or cmd == 0x9:
+        arg = num & 0b_00011111_11111111
+        cmd_SkipColumns(arg)
+        print(f"SkipColumns: 0x{arg // 8:X}")
 
-    elif b1 & 0xF0 == 0xA0:
-        cur_x = num // 8
-        cur_y += 1
-        print(f'{b1:02X} {b2:02X} JumpToColumn: 0x{cur_x:X}')
+    elif cmd == 0xA or cmd == 0xB:
+        arg = num & 0b_00011111_11111111
+        cmd_NextLinePos(arg)
+        print(f"NextLinePos: 0x{arg // 8:X}")
 
-    elif b1 & 0xF0 == 0xC0:
-        for i in range(num // 8):
-            rgb[cur_y * WIDTH * DEPTH + cur_x * DEPTH + 0] = cur_color[0]
-            rgb[cur_y * WIDTH * DEPTH + cur_x * DEPTH + 1] = cur_color[1]
-            rgb[cur_y * WIDTH * DEPTH + cur_x * DEPTH + 2] = cur_color[2]
-            cur_x += 1
-        print(f'{b1:02X} {b2:02X} Put: 0x{num // 8:X}')
+    elif cmd == 0xC:
+        arg = num & 0b_00001111_11111111
+        cmd_PutSpan(arg)
+        print(f"PutSpan: 0x{arg // 8:X}")
+
+    elif cmd == 0xF:
+        arg = num & 0b_00001111_11111111
+        cmd_JumpToLine(arg)
+        print(f"JumpToLine: 0x{arg:X}")
 
     else:
-        print(f'{b1:02X} {b2:02X}')
+        print("")
 
 # save the result as PNG image
 im = Image.frombuffer('RGB', (640, 400), rgb, 'raw', 'RGB', 0, 1)
