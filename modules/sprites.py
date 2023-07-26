f3289f95a1f20bc38ff3325e8c041a3a65001465
Josuah Demangeon <me@josuah.net>
2023-07-26 15:45:36 -0400

    sprites: remove unneeded content

--- modules/sprites.py
+++ modules/sprites.py
@@ -2,81 +2,13 @@ import fpga
 import display
 import math
 import struct
-display.brightness(0)
 
-a = []
-b = []
-for i in range(8):
-    a.extend([0xA0,0x00,0xA0,0xFF])
-    b.extend([0x00,0xA0,0xA0,0xFF])
-for i in range(8):
-    a.extend([0x00,0xA0,0xA0,0xFF])
-    b.extend([0xA0,0x00,0xA0,0xFF])
-for i in range(8):
-    a.extend([0xA0,0x00,0xA0,0xFF])
-    b.extend([0x00,0xA0,0xA0,0xFF])
-for i in range(8):
-    a.extend([0x00,0xA0,0xA0,0xFF])
-    b.extend([0xA0,0x00,0xA0,0xFF])
-addr = 0
-for i in range(15):
-    d = [(addr>>24)&0xFF, (addr>>16)&0xFF, (addr>>8)&0xFF, addr&0xFF]
-    if i < 5 or i >= 10:
-        d.extend(a)
-    else:
-        d.extend(b)
-    fpga.write(0x4404,bytes(d))
-    addr += len(a)
-
-for i in range(32):
-    d = [(addr>>24)&0xFF, (addr>>16)&0xFF, (addr>>8)&0xFF, addr&0xFF]
-    for j in range(i):
-        d.extend([0xFF,0xFF,0xFF,0xFF])
-    for j in range(32-i):
-        d.extend([0x00,0x00,0x00,0x00])
-    fpga.write(0x4404,bytes(d))
-    addr += 128
-
-# Describe the sprites layout
-fpga.write(0x4402,bytes([0x00,0x00,0x10,0xf0,0x00,0x00,0x12,0x00,0x00,0x0f]))
-
-id0 = 0
-x0 = 0x0A10
-y0 = 0x00
-
-x0h = x0 >> 8
-x0l = x0 & 0xFF
-
-id1 = 1
-x1 = 0x0890
-y1 = 0x30
-
-x1h = x1 >> 8
-x1l = x1 & 0xFF
-
-fpga.write(0x4403,bytes([0x00,0x00, x0h,x0l,y0,0x10,id0, x1h,x1l,y1,0x10,id1, 0x00, 0xFF,0xFF,0xFF,0xFF]))
 
 SPRITE_NUMBER = 128
 
-sprite_ids = set()
+sprite_id = 0
 sprite_addr = 0x0000
 
-def sprite_new_id():
-    global sprite_ids
-
-    # Get the first ID that is not already the table
-    id = min(set(range(SPRITE_NUMBER)) - sprite_ids)
-
-    # Mark it as used and return it
-    sprite_ids.add(id)
-    return id
-
-def sprite_alloc_mem(size):
-    global sprite_addr
-
-    addr = sprite_addr
-    sprite_addr += size
-    return addr
 
 def sprite_reset():
     global sprite_addr
@@ -89,32 +21,38 @@ def sprite_reset():
 class Sprite:
     def __init__(self, data, width):
         global sprite_addr
+        global sprite_id
 
-        self.width = width
-        self.height = len(data) // 4 // width
-        self.id = sprite_new_id()
-        self.addr = sprite_addr
+        # Validate the parameters
         if len(data) % (4 * width) != 0:
             raise ValueError("data length must formatted as: b'RGBA' * width")
-        if self.id is None:
-            raise MemoryError("not enough sprites slot")
+        if width % 32 != 0:
+            raise ValueError("width must be a multiple of 32")
+        if sprite_id >= SPRITE_NUMBER:
+            raise MemoryError("not enough sprites memory slots")
+
+        # Construct the object
+        self.width = width
+        self.height = len(data) // 4 // width
+        self.addr = sprite_addr
+        self.id = sprite_id
+        sprite_id += 1
 
         # Send the sprite RGBA data to the FPGA: Address, 128 bytes of data
         for slice in [data[i:i + 128] for i in range(0, len(data), 128)]:
             b = bytearray()
-            b.extend(struct.pack('>H', sprite_addr))
+            b.extend(struct.pack('>I', sprite_addr))
             b.extend(slice)
-            print(b)
             fpga.write(0x4404, b)
             sprite_addr += len(slice)
 
         # Send the sprite layout to the FPGA: Width, Height, and Sprite ID
         b = bytearray()
         b.extend(struct.pack('>H', self.id))
-        w = math.ceil(self.width / 32) & 0xF
+        w = (self.width // 32) & 0xF
         h = self.height & 0xFF
-        i = (self.id // 32) & 0xFFFFF
-        b.extend(struct.pack('>I', w << 28 | h << 20 | i << 0))
+        a = (self.addr // 32) & 0xFFFFF
+        b.extend(struct.pack('>I', w << 28 | h << 20 | a << 0))
         print(b)
         fpga.write(0x4402, b)
 
@@ -131,7 +69,7 @@ class Sprite:
         z = z & 0xF
         i = self.id & 0xFFF
 
-        # pack the fields in a 32-bit integer + 8-bit integer: 5-bytes
+        # Pack the fields in a 32-bit integer + 8-bit integer: 5-bytes
         b.extend(struct.pack('>I', x << 20 | y << 8 | z << 4 | i >> 8))
         b.append(i & 0xFF)
 
@@ -143,12 +81,4 @@ class Sprite:
         fpga.write(0x4403, b)
 
 
-from math import sin, cos
-import time
-for i in range(1, 200):
-    i1 = i / 4
-    s1 = (int(cos(i1) * 8 + 8), int(sin(i1) * 100 + 100), 1)
-    i2 = (i + 5) / 4
-    s2 = (int(cos(i2) * 8 + 8), int(sin(i2) * 100 + 100), 1)
-    place((s1, s2))
-    time.sleep(0.2)
+display.brightness(0)
