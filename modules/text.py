041cc018728aa973c72b78eccea07cd2d5ef65ff
Josuah Demangeon <me@josuah.net>
2023-09-09 19:28:16 -0400

    sprites: backport the previous font library's features

--- modules/text.py
+++ modules/text.py
@@ -36,8 +36,10 @@ MIDDLE_CENTER = 7
 MIDDLE_RIGHT = 8
 BOTTOM_RIGHT = 9
 
-SPACE_WIDTH = 20
-INTER_CHAR_WIDTH = 4
+
+# TODO legacy support for Noa Android/iOS app
+FONT_HEIGHT = 48
+FONT_WIDTH = 24
 
 
 class Text:
@@ -47,6 +49,43 @@ class Text:
         self.font = font
         self.x = x
         self.y = y
+        self.justify(justify)
+
+    def justify(self, justify):
+        left = (TOP_LEFT, MIDDLE_LEFT, BOTTOM_LEFT)
+        center = (TOP_CENTER, MIDDLE_CENTER, BOTTOM_CENTER)
+        right = (TOP_RIGHT, MIDDLE_RIGHT, BOTTOM_RIGHT)
+
+        if justify in left:
+            self.x = self.x
+        elif justify in center:
+            self.x = self.x - self.width(self.string) // 2
+        elif justify in right:
+            self.x = self.x - self.width(self.string)
+        else:
+            raise ValueError("unknown justify value")
+
+        top = (TOP_LEFT, TOP_CENTER, TOP_RIGHT)
+        middle = (MIDDLE_LEFT, MIDDLE_CENTER, MIDDLE_RIGHT)
+        bottom = (BOTTOM_LEFT, BOTTOM_CENTER, BOTTOM_RIGHT)
+
+        if justify in top:
+            self.y = self.y
+        elif justify in middle:
+            self.y = self.y - FONT_HEIGHT // 2
+        elif justify in bottom:
+            self.y = self.y - FONT_HEIGHT
+        else:
+            raise ValueError("unknown justify value")
+
+    def move(self, x, y):
+        self.x += int(x)
+        self.y += int(y)
+        return self
+
+    def width(self, string):
+        # TODO legacy support for Noa Android/iOS app
+        return FONT_WIDTH * len(string)
 
     def sprites(self):
         x = self.x
@@ -55,11 +94,19 @@ class Text:
         sprites = list()
         for ch in self.str:
             if ch == ' ':
-                x += SPACE_WIDTH
+                # TODO legacy support for Noa Android/iOS app
+                x += FONT_WIDTH
                 continue
             sprite = self.font.sprite(ord(ch), self.color)
             sprites.append(sprite.draw(x, y, z))
-            x += sprite.active_width + INTER_CHAR_WIDTH
-            y += sprite.height # TODO remove
+
+            # TODO legacy support for Noa Android/iOS app
+            #x += sprite.active_width
+            x += FONT_WIDTH
+
+            # TODO waiting a bugfix
+            y += sprite.height
+
+            # TODO really necessary? Any upper bound?
             z += 1
         return sprites
