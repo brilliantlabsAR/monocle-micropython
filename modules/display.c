c69cbf89f275f646df031776cdfdd6d8ab5b37fd
Josuah Demangeon <me@josuah.net>
2023-10-21 13:12:54 -0400

    display: fix get_font_data arity

--- modules/display.c
+++ modules/display.c
@@ -31,11 +31,11 @@ uint8_t font_data[] = {
 #include "modules/font/ShareTechMonoBitmap-Regular-64.h"
 };
 
-STATIC mp_obj_t get_font_data(mp_obj_t brightness)
+STATIC mp_obj_t get_font_data(void)
 {
     return mp_obj_new_bytes(font_data, sizeof(font_data));
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(get_font_data_obj, &get_font_data);
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(get_font_data_obj, &get_font_data);
 
 STATIC mp_obj_t display_brightness(mp_obj_t brightness)
 {
